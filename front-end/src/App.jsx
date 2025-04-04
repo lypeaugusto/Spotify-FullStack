9a8664da7a446b40daec42d56daf217375b6b505
lypeaugusto <felypeaugusto2010@gmail.com>
2025-04-04 17:34:35 -0300

    - jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,7 @@
 import 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './pages/Home.jsx';
+import Home from './pages/Home';
 import Artists from './pages/Artists';
 import Artist from './pages/Artist';
 import Songs from './pages/Songs';
