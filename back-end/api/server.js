83cfcd51e7158012e366e934220b4d2f8a6445fb
lypeaugusto <felypeaugusto2010@gmail.com>
2025-04-07 18:08:10 -0300

    LINK 4

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { db } from './connect.js';
 import path from "path";    
+import fs from 'fs';
 
 const __dirname = path.resolve();
 
@@ -47,11 +48,15 @@ app.get('/api/artists', async (request, response) => {
 app.use(express.static(path.join(__dirname, '../../front-end/dist')));
 
 app.get('*', async (request, response) => {
-  {
-      
-    response.sendFile(path.join(__dirname, '../../front-end/dist/index.html'));
-  } 
+  const indexPath = path.join(__dirname, '../front-end/dist/index.html');
+  if (fs.existsSync(indexPath)) {
+    response.sendFile(indexPath);
+  } else {
+    console.error(`Arquivo não encontrado: ${indexPath}`);
+    response.status(404).send('Arquivo index.html não encontrado. Certifique-se de que o front-end foi construído.');
+  }
 });
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
