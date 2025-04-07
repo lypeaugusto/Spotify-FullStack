b1f8b2a466c89254670c7dc55d49bd732396252b
lypeaugusto <felypeaugusto2010@gmail.com>
2025-04-07 16:47:38 -0300

    link 2

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-
 const NODE_ENV = 'development'; // Replace with 'production' for production builds
-const URL = NODE_ENV === 'development'  ? 'https://spotify-fullstack-gdhj.onrender.com/api': 
-;
+const URL = NODE_ENV === 'development' 
+  ? 'https://spotify-fullstack-gdhj.onrender.com/api' 
+  : 'https://spotify-fullstack-gdhj.onrender.com/api'; // Replace with your production URL
 
 const responseArtist = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
 
 export const artistArray = responseArtist.data;
-export const songsArray = responseSongs.data;    
+export const songsArray = responseSongs.data;
 
-    console.log(responseSongs.data);
+console.log(responseSongs.data);
