c180162db816658ecc3d5128603aa86391959aba
lypeaugusto <felypeaugusto2010@gmail.com>
2025-04-07 16:24:04 -0300

    build

--- front-end/src/pages/ArtistTEMP.jsx
+++ front-end/src/pages/ArtistTEMP.jsx
@@ -2,7 +2,7 @@ import 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 import { Link, useParams } from 'react-router-dom';
-import SongList from '../components/Songlist';
+import SongList from '../components/Songlist.jsx';
 import { artistArray } from '../assets/database/artists';
 import { songsArray } from '../assets/database/songs';
 
