35496070ee38d893852eeaaba5f42c84ef6c60c6
lypeaugusto <felypeaugusto2010@gmail.com>
2025-04-09 20:37:00 -0300

    Update HomeTEMP.jsx

--- front-end/src/pages/HomeTEMP.jsx
+++ front-end/src/pages/HomeTEMP.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import App from '../components/App'; // Adjust the path as needed
+import Main from '../components/Main';
 
 const Home = () => {
   return (
-    <App />
+    <Main />
   );
 };
 
