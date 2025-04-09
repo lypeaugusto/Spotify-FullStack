edf5d506eec5bf2db7dd43e19e186f564f456489
lypeaugusto <felypeaugusto2010@gmail.com>
2025-04-09 17:08:15 -0300

    caminho 2

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -47,8 +47,8 @@ app.get('/api/artists', async (request, response) => {
 
 app.use(express.static(path.join(__dirname, '../../front-end/dist')));
 
-app.get('*', (request, response) => {
-  const indexPath = path.join(__dirname, '../../front-end/dist/index.html'); // Corrigido o caminho
+app.get('*', async (request, response) => {
+  const indexPath = path.join(__dirname, '../front-end/dist/index.html');
   if (fs.existsSync(indexPath)) {
     response.sendFile(indexPath);
   } else {
