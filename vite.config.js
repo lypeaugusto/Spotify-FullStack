2204f04205ebc76a47eeb21225328daba2a7b525
lypeaugusto <felypeaugusto2010@gmail.com>
2025-04-09 21:27:29 -0300

    work 3000

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: 'Spotify',
+        },
+      },
+    }),
+  ],
   build: {
-    outDir: '../dist', 
+    outDir: '../dist',
     target: 'esnext',
+    manifest: true, // Gera um manifest.json para mapear os arquivos
   },
   base: './', // Define o caminho base relativo para os recursos
 });
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/indeex.html', // Caminho para o template original
+      inject: 'body',
+    }),
+  ],
+  output: {
+    filename: 'assets/[name]-[contenthash].js', // Gera arquivos com hash
+    path: __dirname + '/dist',
+    clean: true, // Limpa a pasta dist antes do build
+  },
+};
