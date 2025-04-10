fa5f00f758f3cef740401392b1e29bd3e7a823be
lypeaugusto <felypeaugusto2010@gmail.com>
2025-04-09 22:08:13 -0300

    work 4400

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,21 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   root: 'front-end', // Define o diretório raiz como 'front-end'
-  base: '/', // Define caminhos absolutos no HTML
+  base: './', // Define o caminho base como relativo
   plugins: [react()],
   build: {
     outDir: 'dist', // Saída da build será em 'front-end/dist'
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/index.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name][extname]',
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
       },
     },
+    chunkSizeWarningLimit: 1000, // Ajustar o limite de tamanho do chunk
   },
-  publicDir: 'public', // Define um diretório fixo para arquivos públicos
+  
 });
